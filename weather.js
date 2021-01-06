6978e26c48a0c853ac1f5f8136e5378540bd71d3
Devesh RB <72264218+DeveshRB@users.noreply.github.com>
2021-01-06 19:13:40 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -6,7 +6,7 @@ class Weather {
 
     // Fetch weather from api
     async getWeather(){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`)
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=${this.apiKey}`)
 
         const responseData = await response.json();
 
